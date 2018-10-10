2619ebb6ec767b0ba06a4a0658866f38ff3e5e50
Arun Rao <arun@Aruns-MacBook-Air.local>
2018-10-09 21:11:05 -0400

    Wrote tests for and implemented calculateWorkDay()

--- src/main/java/com/kinaxis/interview/Workday.java
+++ src/main/java/com/kinaxis/interview/Workday.java
@@ -1,13 +1,45 @@
 package com.kinaxis.interview;
 
+import java.util.*;
+
 /**
  * Hello world!
  *
  */
-public class Workday
-{
-    public static void main( String[] args )
-    {
+public class Workday {
+
+    private static boolean isWeekend(Calendar calendar){
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        if (dayOfWeek==1 || dayOfWeek== 7){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public static Date calculateWorkday(final Date startDate, final int numberOfWorkdays) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(startDate);
+
+        while (isWeekend(calendar)){
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+        for (int i=0; i<numberOfWorkdays; i++){
+            while(isWeekend(calendar)){
+                calendar.add(Calendar.DAY_OF_WEEK, 1);
+            }
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+        while (isWeekend(calendar)){
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+        }
+
+        Date retval = calendar.getTime();
+        return retval;
+    }
+
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
--- src/test/java/com/kinaxis/interview/WorkdayTest.java
+++ src/test/java/com/kinaxis/interview/WorkdayTest.java
@@ -2,14 +2,18 @@ package com.kinaxis.interview;
 
 import org.junit.Before;
 import org.junit.Test;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 
 /**
  * Unit test for simple App.
  */
-public class WorkdayTest
-{
+public class WorkdayTest {
 
     private Workday classUnderTest;
 
@@ -19,8 +23,46 @@ public class WorkdayTest
     }
 
     @Test
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testCalculateWorkday() throws ParseException{
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        //sanity Test when startDate is not a workday
+        Date startDate = sdf.parse("2018-10-13");
+        Date date = sdf.parse("2018-10-15");
+        //assertEquals(date, Workday.calculateWorkday(startDate, 0));
+
+        //sanity test when startDate is a workday
+        startDate = sdf.parse("2018-10-09");
+        assertEquals(startDate, Workday.calculateWorkday(startDate, 0));
+
+        //returning workdays within same week
+        date = sdf.parse("2018-10-10");
+        assertEquals(date, Workday.calculateWorkday(startDate, 1));
+        date = sdf.parse("2018-10-11");
+        assertEquals(date, Workday.calculateWorkday(startDate, 2));
+        date = sdf.parse("2018-10-12");
+        assertEquals(date, Workday.calculateWorkday(startDate, 3));
+
+        //returning workdays after weekend (following week)
+        date = sdf.parse("2018-10-15");
+        assertEquals(date, Workday.calculateWorkday(startDate, 4));
+        date = sdf.parse("2018-10-16");
+        assertEquals(date, Workday.calculateWorkday(startDate, 5));
+
+        //returning workdays after 2 weekends
+        date = sdf.parse("2018-10-22");
+        assertEquals(date, Workday.calculateWorkday(startDate, 9));
+        date = sdf.parse("2018-10-23");
+        assertEquals(date, Workday.calculateWorkday(startDate, 10));
+
+        //returning workdays in the following month
+        date = sdf.parse("2018-11-01");
+        assertEquals(date, Workday.calculateWorkday(startDate, 17));
+        date = sdf.parse("2018-11-02");
+        assertEquals(date, Workday.calculateWorkday(startDate, 18));
+
+        //returning workdays in the following month after a weekend in the month
+        date = sdf.parse("2018-11-05");
+        assertEquals(date, Workday.calculateWorkday(startDate, 19));
     }
 }
